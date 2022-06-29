7f11a62ba5b718912c6a47aefb8e11ae0a28e230
officialsahoo <sahooramakanta575@gmail.com>
2022-06-29 10:33:48 +0530

    Multiple insert

--- app.js
+++ app.js
@@ -26,21 +26,61 @@ const userSchema = new mongoose.Schema({
 //const User here works as a class so its also written in camel case
 const User = new mongoose.model("User", userSchema);
 
+
+//For inserting single user
+// const createuser = async()=>{
+//     try{
+//         const userDetails = new User({
+//             name:"JKL",
+//             age:96,
+//             mob:5260002589,
+//             active:true
+//         })
+
+//         const result = await userDetails.save();
+//         console.log(result);
+//     }catch(err){
+//         console.log(err);
+//     }
+// }
+//For inserting single user
+
+
+// For inserting multiple user
 const createuser = async()=>{
     try{
-        const userDetails = new User({
-            name:"JKL",
-            age:96,
-            mob:5260002589,
+        const user1 = new User({
+            name:"MNO",
+            age:36,
+            mob:8596201254,
+            active:true
+        })
+        const user2 = new User({
+            name:"PQR",
+            age:20,
+            mob:9856320214,
+            active:true
+        })
+        const user3 = new User({
+            name:"STU",
+            age:99,
+            mob:8952021478,
+            active:true
+        })
+        const user4 = new User({
+            name:"VWX",
+            age:88,
+            mob:2562369856,
             active:true
         })
 
-        const result = await userDetails.save();
+        const result = await User.insertMany([user1,user2,user3,user4]);
         console.log(result);
-    }catch(err){
+    } catch(err){
         console.log(err);
     }
 }
+// For inserting multiple user
 
 createuser()
 
